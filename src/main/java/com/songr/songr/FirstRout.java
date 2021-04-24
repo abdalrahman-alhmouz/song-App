7335034f03591db2b2e953f3af9a4cfab2092172
abdalrahman-alhmouz <abood.alhmouz121@gmail.com>
2021-04-24 17:22:52 +0300

    final comit

--- src/main/java/com/songr/songr/FirstRout.java
+++ src/main/java/com/songr/songr/FirstRout.java
@@ -16,6 +16,18 @@ public class FirstRout {
     @Autowired
     AlbumReposity albumReposity;
 
+@GetMapping("/")
+public String homePage(){
+    return "hello.html";
+}
+
+    @GetMapping("/hello")
+    public String string(@RequestParam(name="name", required=false, defaultValue="There") String name, Model m){
+        m.addAttribute("name", name.toUpperCase());
+        return "hello.html";
+    }
+
+
     @GetMapping("/album")
     public String getAlbums(Model m){
         m.addAttribute("album",albumReposity.findAll());
@@ -30,8 +42,12 @@ public class FirstRout {
 
 
 
-    @PostMapping("/AddAlbum")
-    public RedirectView addAlpum(String title, String artist, String imageUrl, double length, int songCount){
+    @PostMapping("/album")
+    public RedirectView addAlpum(@RequestParam(value = "title") String title,
+                                 @RequestParam(value = "artist") String artist,
+                                 @RequestParam(value = "imageUrl") String imageUrl,
+                                 @RequestParam(value = "length") double length,
+                                 @RequestParam(value = "songCount")  int songCount){
         Album album=new Album(title,artist,imageUrl,length,songCount);
         albumReposity.save(album);
         return new RedirectView("/album");}
--- src/main/java/com/songr/songr/SongControler.java
+++ src/main/java/com/songr/songr/SongControler.java
@@ -5,10 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -33,13 +30,16 @@ public class SongControler {
 
 
     @PostMapping("/SongAdd")
-    public RedirectView addSong(String title, int length, int trackNumber, Integer id){
+    public RedirectView addSong(@RequestParam(value = "title") String title,
+                                @RequestParam(value = "length") int length,
+                                @RequestParam(value = "trackNumber") int trackNumber,
+                                @RequestParam(value = "id") Integer id){
         Album album=albumReposity.findById(id).get();
         Song song=new Song( title,  length, trackNumber,album);
         songRepository.save(song);
         return new RedirectView("/Song");
     }
-        @GetMapping("/Songs.html")
+        @GetMapping("/AddSongS")
     public String getPage(){
         return "Songs.html";
     }
